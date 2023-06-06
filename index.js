f745952abd314a629c94edaaf73c76809715ee97
armaan48 <54315881+armaan48@users.noreply.github.com>
2023-06-06 08:49:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const server = http.createServer(app);
 
 const io = new Server(server , {
     cors:{
-        origin: "https://chatapp48.netlify.app"
+        origin: "https://chatapp48.netlify.app",
+    methods: ["GET", "POST"]
     }
 } ) 
 
