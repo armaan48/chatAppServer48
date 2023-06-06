c1fa4562365855f1d6a9e5e39cf6c27f16bc89de
armaan48 <54315881+armaan48@users.noreply.github.com>
2023-06-06 07:48:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+var http = require('http');
+const cors = require("cors");
+var {Server} = require('socket.io');
+app.use(cors());
+ 
+const server = http.createServer(app);
+
+const io = new Server(server , {
+    cors:{
+        origin: "http://localhost:3000"
+    }
+} ) 
+
+
+app.get("/", (req,res)=>{
+    res.send("running server well and good");
+})
+
+io.on('connection', function (socket) {
+
+    socket.on("join-room" , (data)=>{
+        console.log(socket.id + " joinedRoom: " + data);
+        socket.join(data);
+    })
+
+    socket.on('send-message', (data) => {
+        socket.to(data.room).emit("recieve-message" , data);
+    });
+    socket.on('disconnect', function () {
+        delete listUname[username];
+        console.log('user ' + username + '  disconnected');
+
+    });
+
+
+});
+server.listen(3001,  ()=>{
+    console.log('listening on *:3001');
+});
